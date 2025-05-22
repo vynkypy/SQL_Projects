cf726783df6ed9764fa5547e386ce7ee7b8cc797
vynkypy <57390573+vynkypy@users.noreply.github.com>
2025-05-22 13:31:11 +0530

    Are any transactions linked to blacklisted customers

---
+++ Financial Fraud & AML Surveillance Database/Financial Fraud_ Q1.sql
@@ -0,0 +1,6 @@
+-- Are any transactions linked to blacklisted customers or sanctioned entities?
+
+select c.Name, c.Country, a.AccountType, a.Status,a.Balance, t.Amount as Transaction_Amount, t.Currency from Accounts A join Customers c 
+on a.CustomerID = c.CustomerID
+join Transactions t on t.FromAccountID = a.AccountID
+where c.IsBlacklisted = 1
